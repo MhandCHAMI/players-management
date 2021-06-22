5709cb471356de67c04b91cebb0e16c3c5e97735
MhandCHAMI <ch.mhand@gmail.com>
2021-06-22 20:45:02 +0200

    initialisation du projet

---
+++ src/test/java/com/cmconsulting/playersManagement/PlayersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmconsulting.playersManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
