396d695be1b0a752cd7ef779b5b1bda7298f8cc1
MhandCHAMI <ch.mhand@gmail.com>
2022-11-01 15:40:12 +0100

    modification 1

--- src/main/java/com/cmconsulting/playersManagement/domain/Equipe.java
+++ src/main/java/com/cmconsulting/playersManagement/domain/Equipe.java
@@ -20,7 +20,6 @@ public class Equipe implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
